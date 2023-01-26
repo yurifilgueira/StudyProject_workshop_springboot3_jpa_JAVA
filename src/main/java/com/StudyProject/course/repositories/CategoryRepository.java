5b13055e07d579e412ba7ca191df4ee2dc1fb563
yurifilgueira <yurimedeiros141@gmail.com>
2023-01-26 18:15:12 -0300

    Category

---
+++ src/main/java/com/StudyProject/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.StudyProject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.StudyProject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+	
+}
